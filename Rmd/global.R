ae14c991c95738347e8a7bb80d059df0537880fc
fmichelsendis <franco.michelsendis@gmail.com>
2018-02-20 18:13:40 +0100

    Adding csv folder and other minor updates

---
+++ Rmd/global.R
@@ -0,0 +1,63 @@
+
+# These are the global includes in Shiny web application.
+# & First import of data
+# MAPPING app
+# Authors : F. Michel-Sendis & L. Fiorito
+#
+library(shiny)
+library(stringr)
+library(plotly)
+library(ggplot2)
+library(shinydashboard)
+library(shinythemes)
+library(shinyjs)
+library(VIM)
+library(rsconnect)
+library(reshape2)
+library(data.table)
+library(DT)
+library(dtplyr)
+library(dplyr)
+library(splitstackshape)
+library(rmarkdown)
+library(flexdashboard)
+
+# set environment variable to pass proxy
+#Sys.setenv(HTTPS_PROXY="http://proxy-vip1.oecd-nea.org:3128")
+
+
+
+df <- fread('csv/testdata.csv', header = TRUE, sep = ",", stringsAsFactors = FALSE)
+# mats contains 562 (JEFF.33 correspondences between MAT and Z,A,M)
+mats <- fread('csv/matzsymam.csv', header = TRUE, sep = ",", stringsAsFactors = FALSE)
+df <- merge(df, mats)
+df$MFMT <- paste(df$MF*1000+df$MT)
+
+mts<-fread('csv/MTs.csv',header = TRUE, sep = ",", stringsAsFactors = TRUE)
+mfs<-fread('csv/MF.csv',header = TRUE, sep = ",", stringsAsFactors = TRUE)
+
+df<-merge(df, mfs, by = "MF")
+df<-merge(df, mts, by = "MT")
+
+
+# add fields release (LIBVER) and origin release (LIBVERORIG)
+df$LIBVERORIG <- paste(df$LIBORIG,"-",df$VERORIG, sep='')
+df$LIBVER <- paste(df$LIB,"-",df$VER, sep='')
+
+df$SYMA<-paste(df$X,"-",df$A, sep='')
+df$SYMAM<-paste(df$X,"-",df$A,"-",df$M, sep='')
+df <- df[order(df$X),]
+
+df$MF <- factor(df$MF)
+df$MT <- factor(df$MT)
+
+
+colors<-fread("csv/my_colors.csv")
+#needs to be a named vector :
+my_colors<- setNames(as.character(colors$LIBVERCOLOR), as.character(colors$LIBVER))
+
+libdates <- fread('csv/libdate.csv', header = TRUE, sep = ",", stringsAsFactors = TRUE)
+libdates$LIBVER<-paste(libdates$LIB,libdates$VER, sep="-")
+libdates<- setNames(as.character(libdates$YY), as.character(libdates$LIBVER))
+
+ 
--- Rmd/wrapper.R
+++ Rmd/wrapper.R
@@ -3,27 +3,22 @@
 # in the df dataframe in global.R
 # Authors : F. Michel-Sendis & L. Fiorito 
 #
-source('global_vars.R')
-# Define report tibble with output filenames and parameters
+ 
 
-mylib<-"JEFF-3.3"
+mylib<-"JEFF-3.3" 
 
-#df2<-filter(df, LIBVER==mylib)
-
-df2<-df%>%subset(LIBVER==mylib & X=="Pu")
-
-
+df2<-df%>%subset(LIBVER==mylib & X=="U")
 df2$SYMAMLIBVER<-paste(df2$SYMAM,"-",df2$LIB,"-",df2$VER, sep="")
 
+# Define report tibble with output filenames and parameters
 reports <- tibble(
   SYMAMLIBVER = unique(df2$SYMAMLIBVER),
   SYMAMS = unique(df2$SYMAM),
-  myparams = purrr::map(SYMAMLIBVER, ~ list(SYMAMLIBVER= .)),
+  params = purrr::map(SYMAMLIBVER, ~ list(SYMAMLIBVER= .)),
   dirname = mylib,
   filename = paste(SYMAMS,".html", sep="")
 )
-
 # Parametrized rendering of the lib_content.Rmd markdown script using reports parameters as inputs
 reports %>%
-  select(output_dir = dirname, output_file = filename, params= myparams) %>%
+  select(output_dir = dirname, output_file = filename, params) %>%
   purrr::pwalk(rmarkdown::render, input = "Map.Rmd")
