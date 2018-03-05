aa82e7dfc6907057a64c536946750ebf5488fa04
Luca Fiorito <lucafiorito.11@gmail.com>
2018-03-05 10:42:03 +0100

    minor changes

--- JEFF33_vs_JEFF32/new_files.R
+++ JEFF33_vs_JEFF32/new_files.R
@@ -5,6 +5,6 @@ library(data.table)
 df <-fread('JEFF33_vs_JEFF32.csv', header = TRUE, sep = ",", stringsAsFactors = TRUE) %>% # RELATIVE PATH
   subset(MT!=451) %>%  # REMOVE MF1MT451
   dplyr::group_by(MAT) %>% 
-  dplyr::summarize(NEW=all(VERORIG==3.3)) %>%
-  subset(NEW==TRUE) %>% # SET TO FALSE TO HAVE MATS THAT HAVE NOT CHANGED
-  merge(fread('../Rapp/csv/matzsymam.csv', header = TRUE, sep = ",", stringsAsFactors = TRUE)) # MERGE TO HAVE MASS, CHARGE, ETC
+  dplyr::summarize(OLD=all(VERORIG==3.2)) %>%
+  subset(OLD==TRUE) %>% # SET TO FALSE TO HAVE MATS THAT HAVE NOT CHANGED
+  merge(fread('../csv/matzsymam.csv', header = TRUE, sep = ",", stringsAsFactors = TRUE)) # MERGE TO HAVE MASS, CHARGE, ETC
