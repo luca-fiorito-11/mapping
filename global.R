3c4b43e64825792892c1933addb9b9ddeab99c80
franco <franco.michelsendis@gmail.com>
2018-03-06 18:28:02 +0100

    Update in Lib, add covariance mapping

--- global.R
+++ global.R
@@ -1,5 +1,5 @@
-# These are the global includes in Shiny web application.
-# & First import of data
+# These are the global includes for all Rmd applications
+# First import of data
 # MAPPING app
 # Authors : F. Michel-Sendis & L. Fiorito
 #
@@ -25,13 +25,13 @@ library(knitr)
 #library(rsconnect)
 #library(reshape2)
 
-# install packages if not already present : 
-# 
-# list.of.packages <- c("plotly", "shinythemes", "shinyjs",
-#                       "data.table", "DT", "dplyr", "flexdashboard")
-# 
-# new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-# if(length(new.packages)) install.packages(new.packages)
+# install packages if not already present :
+
+list.of.packages <- c("plotly", "shinythemes", "shinyjs",
+                      "data.table", "DT", "dplyr", "flexdashboard")
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 
 
 
@@ -87,52 +87,37 @@ returnHumanReadableHalfLife <- function(seconds){
 gpath<-"../../csv/"
 #gpath<-"https://raw.githubusercontent.com/luca-fiorito-11/mapping/r-app/csv/"
 
+# dataframe containing 562 (JEFF.33 correspondences between MAT and Z,A,M)
+mats <- fread(paste0(gpath,'matzsymam.csv'))#,
+              # header = TRUE,
+              # sep = ",",
+              # stringsAsFactors = FALSE)
+
+# dataframe containing descriptions for each MF
+mts<-fread(paste0(gpath,'MTs.csv'))  
+
+# dataframe containing descriptions for each MT
+mfs<-fread(paste0(gpath,'MF.csv'))  
+
 # dataframe coming from the mapping run
-df <- fread(paste0(gpath,'CHUNKS-JEFF.csv'),
-            header = TRUE,
-            sep = ",",
-            stringsAsFactors = FALSE)
+df <- fread(paste0(gpath,'CHUNKS-JEFF.csv')) 
+df$LIBVERORIG <- paste(df$LIBORIG,"-",df$VERORIG, sep='')
+df$LIBVER <- paste(df$LIB,"-",df$VER, sep='')
 
-# dataframe containing 562 (JEFF.33 correspondences between MAT and Z,A,M)
-mats <- fread(paste0(gpath,'matzsymam.csv'),
-              header = TRUE,
-              sep = ",",
-              stringsAsFactors = FALSE)
+# Merge dataframes
+df <- df %>% merge(mats, by = "MAT", all.x = TRUE)  
 
-# dataframe containing 562 (JEFF.33 correspondences between MAT and Z,A,M)
-decay <- fread(paste0(gpath,'decay_data_jeff33.csv'),
-              header = TRUE,
-              sep = ",",
-              stringsAsFactors = FALSE)
+df <- df %>% merge(mfs, by = "MF", allow.cartesian = T)
+
+df <- df %>% merge(mts, by = "MT", all.x = TRUE, allow.cartesian = T) 
+  
+
+decay <- fread(paste0(gpath,'decay_data_jeff33.csv'))
 decay$HalfLife<-lapply(decay$HL, function(x)returnHumanReadableHalfLife(x)) 
 decay$HL<-lapply(decay$HL, function(x){formatC(x, format="e", digits=1)}) 
 
-
-# dataframe containing descriptions for each MF
-mts<-fread(paste0(gpath,'MTs.csv'),
-           header = TRUE,
-           sep = ",",
-           stringsAsFactors = TRUE)
-
-# dataframe containing descriptions for each MT
-mfs<-fread(paste0(gpath,'MF.csv'),
-           header = TRUE,
-           sep = ",",
-           stringsAsFactors = TRUE)
-
-# Merge dataframes
-df <- df %>%
-  merge(mats) %>%
-  # use allow.cartesian as precautionary measure when the result of duplicating keys gets bigger than max(nrow(x), nrow(i))
-  merge(mfs, by = "MF", allow.cartesian = T) %>%
-  merge(mts, by = "MT", allow.cartesian = T)
-
 # Add unique ID MFMT as new column
 df$MFMT <- paste(df$MF*1000+df$MT)
-
-# add fields release (LIBVER) and origin release (LIBVERORIG)
-df$LIBVERORIG <- paste(df$LIBORIG,"-",df$VERORIG, sep='')
-df$LIBVER <- paste(df$LIB,"-",df$VER, sep='')
 
 df$SYMA <- paste(df$X,"-",df$A, sep='')
 df$SYMAM <- paste(df$X,"-",df$A,"-",df$M, sep='')
@@ -141,11 +126,12 @@ df <- df[order(df$X),]
 df$MF <- factor(df$MF)
 df$MT <- factor(df$MT)
 
-# dataframe containing colors associated to eahc lib version
+# dataframe containing colors associated to each lib version
 colors <- fread(paste0(gpath,'my_colors.csv'))
 #needs to be a named vector :
 my_colors<- setNames(as.character(colors$LIBVERCOLOR), as.character(colors$LIBVER))
 
+# dates for libraries release, can be used to establish precedence 
 libdates <- fread(paste0(gpath,'libdate.csv'), header = TRUE, sep = ",", stringsAsFactors = TRUE)
 libdates$LIBVER<-paste(libdates$LIB,libdates$VER, sep="-")
 libdates<- setNames(as.character(libdates$YY), as.character(libdates$LIBVER))
